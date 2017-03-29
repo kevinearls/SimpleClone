ef5bed2d2e8c02a7a6edc3626f7352f0b27f2f1e
Kevin Earls <kevin@kevinearls.com>
2017-03-29 13:19:12 +0200

    Added .gitignore, simple test to find hostname

---
+++ src/test/java/com/kevinearls/Simple/InetTest.java
@@ -0,0 +1,16 @@
+package com.kevinearls.Simple;
+
+import org.junit.Test;
+
+import java.net.InetAddress;
+
+/**
+ * Created by kearls on 29/03/2017.
+ */
+public class InetTest {
+    @Test
+    public void getHostnameTest() throws Exception {
+        String hostName = InetAddress.getLocalHost().getHostName();
+        System.out.println("HOSTNAME [" + hostName + "");
+    }
+}
